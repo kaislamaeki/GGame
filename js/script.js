4261d045979cb442f7f6f57c41704808fd96f1c6
sergi <sergi.qoniashvili@gmail.com>
2024-01-22 04:29:49 +0100

    Update 09 Demo ist schon fertig

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-let menu = document.querySelectorAll('#menu-icon');
-let navlist = document.querySelectorAll('.navlist');
+document.addEventListener('DOMContentLoaded', function () {
+    let menu = document.querySelector('#menu-icon');
+    let navlist = document.querySelector('.navlist');
 
-menu.onclick = () =>{
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
-}
+    if (menu && navlist) {
+        menu.addEventListener('click', () => {
+            menu.classList.toggle('open');
+            navlist.classList.toggle('open');
+        });
+    } else {
+        console.error('Fehler: #menu-icon oder .navlist wurde nicht gefunden.');
+    }
+});
+
+const sr =ScrollReveal({
+    distance: '65px',
+    duration: 2600 ,
+    delay: 450,
+    reset: true,
+})
+sr.reveal('.hero-text',{delay: 200, origin: 'top'})
+sr.reveal('.hero-img',{delay: 450, origin: 'top'})
+sr.reveal('.icons',{delay: 500, origin: 'left'})
+sr.reveal('.scroll-down',{delay: 500, origin: 'right'})
